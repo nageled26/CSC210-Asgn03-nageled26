52306ca3d5c83ab3cb6ea37fa43257f2f1462f78
nageled26 <31517134+nageled26@users.noreply.github.com>
2017-09-12 12:28:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dnage/landmarkscrapbook/Navigation.java
@@ -0,0 +1,23 @@
+package com.example.dnage.landmarkscrapbook;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Navigation extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+
+
+
+
+}
